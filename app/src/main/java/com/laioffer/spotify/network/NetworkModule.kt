d93d6c527d6baf9134e3f7304c85d27ac00d72ff
Dennis Lam <hinyueng.lam@gmail.com>
2024-05-13 22:28:38 -0700

    Initialization

--- app/src/main/java/com/laioffer/spotify/network/NetworkModule.kt
+++ app/src/main/java/com/laioffer/spotify/network/NetworkModule.kt
@@ -1,20 +1,32 @@
 package com.laioffer.spotify.network
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
-// object is a singleton class in Kotlin
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
-    private const val BASE_URL = "http://10.0.2.2:8080/" // localhost address for android emulator
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
-    // class under object is static
+    @Provides
+    @Singleton
     fun provideRetrofit(): Retrofit {
-        // return a new Retrofit object every time
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(OkHttpClient())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(retrofit: Retrofit): NetworkApi {
+        return retrofit.create(NetworkApi::class.java)
     }
 }
